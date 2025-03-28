ace99168093eee6f1be003ecffb0c1461dc56d9f
22UCS097 <ravikumar628763@gmail.com>
2025-03-29 02:22:34 +0530

    navbar creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import BuyCredit from './pages/BuyCredit'
+import Result from './pages/Result'
+import Navbar from './components/Navbar'
 
 export default function App() {
   return (
-    <div className='text-red-500'>
-       Hello
+    <div className=' bg-slate-500 px-4 sm:px-10 md:px-14 lg:px-28
+      min-h-screen'>
+       <Navbar/>
+      <Routes>
+         <Route path="/"  element={<Home/>} />
+         <Route path="/buy"  element={<BuyCredit/>} />
+         <Route path="/result"  element={<Result/>} />
+      </Routes>
+
     </div>
   )
 }
