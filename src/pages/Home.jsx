b36c341fb1dcdc990bba3dac5dd0eeebb245776d
22UCS097 <ravikumar628763@gmail.com>
2025-03-30 00:39:54 +0530

    framer motion addition and  some functionality of login

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
-import GenerateBtn from '../components/GenerateBtn'
+
 
 export default function Home() {
+
+  
   return (
     <div>
       <Header/>
-      <GenerateBtn/>
+     
     </div>
   )
 }
